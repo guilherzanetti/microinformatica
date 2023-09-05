4fcaeecd25b5ae6e5d00ff0d41b24721ace46e5b
Guilherme Zanetti <94937630+guilherzanetti@users.noreply.github.com>
2023-09-05 16:51:47 -0300

    variavel tipo numérica

--- BUL03-DetalhesConsole/scriptJavaScript.js
+++ BUL03-DetalhesConsole/scriptJavaScript.js
@@ -3,4 +3,22 @@ console.log("Primeira Linha");
 console.log("Segunda Linha");
 
 // ---- A linguagemé "Case Sensitive"
-console.LOG("Terceira Linha")
+// O código a seguir gera um erro
+// console.LOG("Terceira Linha");
+
+// ---- Teste com variável
+
+$x = 1;
+console.log($x);
+
+// ---- Teste com erro
+
+// console.log($X);
+
+// ---- tipagem fraca
+
+var $x2 = 2; //variável do tipo numérica
+console.log($x2);
+
+$x2 = 'A' // Mudamos o tipo da variável para "Sring"
+
