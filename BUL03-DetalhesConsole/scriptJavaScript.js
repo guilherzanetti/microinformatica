88583e334f41b309a42cdb633f3bdce8f0285b0c
Guilherme Zanetti <94937630+guilherzanetti@users.noreply.github.com>
2023-09-05 16:35:46 -0300

    DetalhesConsole v2

--- BUL03-DetalhesConsole/scriptJavaScript.js
+++ BUL03-DetalhesConsole/scriptJavaScript.js
@@ -1,3 +1,6 @@
 // ---- O código é executado de cima para baixo
 console.log("Primeira Linha");
 console.log("Segunda Linha");
+
+// ---- A linguagemé "Case Sensitive"
+console.LOG("Terceira Linha")
